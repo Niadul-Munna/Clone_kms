6d0f9eeebd47b1655271b19c8dedd7c6972227b2
niadul_munna <niadulislammunna@gmail.com>
2024-12-03 19:31:35 +0600

    someUpdate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,12 @@
-import * as React from "react";
-import { type SearchParams } from "@/types";
+import React from "react";
+import SideBar from "./dashboard/page";
 
-import { getValidFilters } from "@/lib/data-table";
-import { Skeleton } from "@/components/ui/skeleton";
-import { DataTableSkeleton } from "@/components/data-table/data-table-skeleton";
-import { DateRangePicker } from "@/components/date-range-picker";
-import { Shell } from "@/components/shell";
-
-import { FeatureFlagsProvider } from "./_components/feature-flags-provider";
-import { TasksTable } from "./_components/tasks-table";
-import {
-  getTaskPriorityCounts,
-  getTasks,
-  getTaskStatusCounts,
-} from "./_lib/queries";
-import { searchParamsCache } from "./_lib/validations";
-
-interface IndexPageProps {
-  searchParams: Promise<SearchParams>;
-}
-
-export default async function IndexPage(props: IndexPageProps) {
-  const searchParams = await props.searchParams;
-  const search = searchParamsCache.parse(searchParams);
-
-  const validFilters = getValidFilters(search.filters);
-
-  const promises = Promise.all([
-    getTasks({
-      ...search,
-      filters: validFilters,
-    }),
-    getTaskStatusCounts(),
-    getTaskPriorityCounts(),
-  ]);
-
+const Home = () => {
   return (
-    <Shell className="gap-2">
-      <FeatureFlagsProvider>
-        <React.Suspense fallback={<Skeleton className="h-7 w-52" />}>
-          <DateRangePicker
-            triggerSize="sm"
-            triggerClassName="ml-auto w-56 sm:w-60"
-            align="end"
-            shallow={false}
-          />
-        </React.Suspense>
-        <React.Suspense
-          fallback={
-            <DataTableSkeleton
-              columnCount={6}
-              searchableColumnCount={1}
-              filterableColumnCount={2}
-              cellWidths={["10rem", "40rem", "12rem", "12rem", "8rem", "8rem"]}
-              shrinkZero
-            />
-          }
-        >
-          <TasksTable promises={promises} />
-        </React.Suspense>
-      </FeatureFlagsProvider>
-    </Shell>
+    <div>
+      <SideBar />
+    </div>
   );
-}
+};
+
+export default Home;
